5be02cfc498b019430d6270d072c400fd6ee5911
alfannnnnnnnnnnnnnnnnnnnnnn <alfakatsuki@gmail.com>
2018-04-18 11:38:12 +0700

    none

--- temporary/hehe.php
+++ temporary/hehe.php
@@ -26,21 +26,21 @@
         });
 
 
-		Schema::create('user', function (Blueprint $table) {
-            $table->string('email');
-            $table->string('password');
-            $table->string('full_name');
-            $table->string('alamat');
-			$table->int('no_hp');
-			$table->longText('about');
-            $table->string('faculty');
-            $table->string('dept');
-            $table->int('reported');
-			$table->float('rating');
+		Schema::create('Book', function (Blueprint $table) {
+            $table->string('id_book')->primary();
+            $table->string('name');
+            $table->string('genre');
+            $table->longText('description');
             $table->timestamps();
         });
-
-        
+		
+		Schema::create('Message', function (Blueprint $table) {
+            $table->string('user_a');
+            $table->string('user_b');
+            $table->longText('message');
+            $table->timestamps();
+        });
+		        
         
     }
     

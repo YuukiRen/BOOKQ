29686835480af36efad04ef302ae951e05827a9e
YuukiRen <reinaldoalvin1@gmail.com>
2018-05-01 09:46:56 +0700

    Revert "view"
    This reverts commit 92b5e598e50361f7bef70b62360af1dad7c150e0.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('nama_lengkap');
+            $table->string('nim')->unique();
+            $table->string('alamat');
             $table->rememberToken();
             $table->timestamps();
         });

fd3c27552a1d801add341964955985a0b22a3e15
a <alfakatsuki@gmail.com>
2018-05-09 01:56:27 +0700

    create search functionality

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -15,9 +15,32 @@ class SearchController extends Controller
 
     public function search(Request $request)
     {
-    	if($request->ajax())
-    	{
-    		$customer;
-    	}
+
+        // $books=Book::all();
+        //membuat paginasi
+        $find_text = $request['query'];
+        $find_cat = $request['category'];
+        if($find_cat !== NULL){
+        $books=\DB::table('books')
+            ->where('category', '=', $find_cat)
+            ->orWhere('LOWER( author )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( title )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( publisher )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( tag )', 'like', '%'.$find_text.'%')
+            ->get()
+            ->paginate(12);
+        }
+        else{
+            $books=\DB::table('books')
+            ->Where('LOWER( author )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( title )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( publisher )', 'like', '%'.$find_text.'%')
+            ->orWhere('LOWER( tag )', 'like', '%'.$find_text.'%')
+            ->get()
+            ->paginate(12);
+        }
+        dd($books);
+        return view('search', compact('books'));
+        
     }
 }

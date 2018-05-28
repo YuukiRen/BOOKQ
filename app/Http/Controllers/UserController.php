edb67da32f9508c54afe1d29dae363341fef693a
a <alfakatsuki@gmail.com>
2018-05-28 10:10:09 +0700

    back to run mode

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,8 +59,6 @@ class UserController extends Controller
         $requests_lend = Transaction::where('id_lender',$id)->get();
         $requests_borrow = Transaction::where('id_booker',$id)->get();
         $activities = Transaction::where('id_booker',$id)->OrWhere('id_lender',$id)->get();
-        $activities = Transaction::where('id_booker',$id)->OrWhere('id_lender',$id)->toSql();
-        dd($activities);
         return view('myProfile', compact('users','requests_lend','requests_borrow','activities','books'));
         
         // dd($users);
